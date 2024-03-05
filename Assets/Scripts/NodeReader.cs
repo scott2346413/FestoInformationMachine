465b65b71384acbc2c2a795a8e2f426d814b25f3
scott2346413 <2346413@swansea.ac.uk>
2024-03-05 13:11:59 +0000

    Project Setup

---
+++ Assets/Scripts/NodeReader.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using realvirtual;
+
+
+public class NodeReader : MonoBehaviour
+{
+
+    [Header("Factory Machine")]
+    public int factoryMachineID;
+    public OPCUA_Interface oPCUAInterface;
+
+    [Header("OPCUA Reader")]
+    public string nodeBeingMonitored;
+    public string nodeID;
+    public string dataFromOPCUANode;
+
+
+    // Subscribe to OPC UA events on start
+    void Start()
+    {
+        oPCUAInterface.EventOnConnected.AddListener(OnInterfaceConnected);
+        oPCUAInterface.EventOnDisconnected.AddListener(OnInterfaceDisconnected);
+    }
+
+    // Method called when the OPC UA interface is connected
+    private void OnInterfaceConnected()
+    {
+        // Subscribe to the specified node and provide the method to call on node change
+        var subscription = oPCUAInterface.Subscribe(nodeID, NodeChanged);
+        dataFromOPCUANode = subscription.ToString();
+
+        Debug.Log("Connected to Factory Machine " + factoryMachineID);
+        Debug.Log(dataFromOPCUANode);
+    }
+
+    // Method called when the OPC UA interface is disconnected
+    private void OnInterfaceDisconnected()
+    {
+        Debug.LogWarning("Factory Machine " + factoryMachineID + " has disconnected");
+    }
+
+    // Method called when the monitored node changes its value
+    public void NodeChanged(OPCUANodeSubscription sub, object value)
+    {
+        dataFromOPCUANode = value.ToString();
+        Debug.Log(dataFromOPCUANode);
+    }
+}
